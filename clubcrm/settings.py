e02ba03f14e369a7c054be4bb1bd1f471a0f68ae
rjokhan <jaidonswift@gmail.com>
2025-08-04 11:27:56 +0500

    Fix: добавлены функции для открытия попапа события

--- clubcrm/settings.py
+++ clubcrm/settings.py
@@ -128,5 +128,5 @@ import os
 
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'core', 'templates')]
 STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # для собранной статики
